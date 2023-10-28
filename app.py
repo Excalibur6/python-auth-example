c7a1efcf86c88bd57e812c4dd9c4d2da1c0b1a01
Your GitHub username <Your GitHub email address>
2023-10-29 01:35:30 +0500

    Starting up the project

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
 from flask import Flask, url_for, redirect, render_template, request, abort
 from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore, \
-    UserMixin, RoleMixin, current_user
+from flask_security import (
+    Security,
+    SQLAlchemyUserDatastore,
+    UserMixin,
+    RoleMixin,
+    current_user,
+)
 from flask_security.utils import hash_password
 import flask_admin
 from flask_admin.contrib import sqla
@@ -11,15 +16,15 @@ from flask_admin import helpers as admin_helpers
 
 # Create Flask application
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+app.config.from_pyfile("config.py")
 db = SQLAlchemy(app)
 
 
 # Define models
 roles_users = db.Table(
-    'roles_users',
-    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+    "roles_users",
+    db.Column("user_id", db.Integer(), db.ForeignKey("user.id")),
+    db.Column("role_id", db.Integer(), db.ForeignKey("role.id")),
 )
 
 
@@ -40,8 +45,9 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
+    roles = db.relationship(
+        "Role", secondary=roles_users, backref=db.backref("users", lazy="dynamic")
+    )
     fs_uniquifier = db.Column(db.String(64), unique=True, nullable=False)
 
     def __str__(self):
@@ -56,9 +62,10 @@ security = Security(app, user_datastore)
 # Create customized model view class
 class MyModelView(sqla.ModelView):
     def is_accessible(self):
-        return (current_user.is_active and
-                current_user.is_authenticated and
-                current_user.has_role('superuser')
+        return (
+            current_user.is_active
+            and current_user.is_authenticated
+            and current_user.has_role("superuser")
         )
 
     def _handle_view(self, name, **kwargs):
@@ -71,24 +78,27 @@ class MyModelView(sqla.ModelView):
                 abort(403)
             else:
                 # login
-                return redirect(url_for('security.login', next=request.url))
+                return redirect(url_for("security.login", next=request.url))
+
 
 # Flask views
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 # Create admin
 admin = flask_admin.Admin(
     app,
-    'Authentication',
-    base_template='my_master.html',
-    template_mode='bootstrap4',
+    "Authentication",
+    base_template="my_master.html",
+    template_mode="bootstrap4",
 )
 
 # Add model views
 admin.add_view(MyModelView(Role, db.session))
 admin.add_view(MyModelView(User, db.session))
+
 
 # define a context processor for merging flask-admin's template context into the
 # flask-security views.
@@ -98,7 +108,7 @@ def security_context_processor():
         admin_base_template=admin.base_template,
         admin_view=admin.index_view,
         h=admin_helpers,
-        get_url=url_for
+        get_url=url_for,
     )
 
 
@@ -114,47 +124,49 @@ def build_sample_db():
     db.create_all()
 
     with app.app_context():
-        user_role = Role(name='user')
-        super_user_role = Role(name='superuser')
+        user_role = Role(name="user")
+        super_user_role = Role(name="superuser")
         db.session.add(user_role)
         db.session.add(super_user_role)
         db.session.commit()
 
         test_user = user_datastore.create_user(
-            first_name='Admin',
-            email='admin@example.com',
-            password=hash_password('admin'),
-            roles=[user_role, super_user_role]
+            first_name="Admin",
+            email="admin@example.com",
+            password=hash_password("admin"),
+            roles=[user_role, super_user_role],
         )
 
-        first_names = [
-            'Harry', 'Amelia', 'Oliver', 'Jack', 'Isabella'
-        ]
-        last_names = [
-            'Brown', 'Smith', 'Patel', 'Jones', 'Williams'
-        ]
+        first_names = ["Harry", "Amelia", "Oliver", "Jack", "Isabella"]
+        last_names = ["Brown", "Smith", "Patel", "Jones", "Williams"]
 
         for i in range(len(first_names)):
-            tmp_email = first_names[i].lower() + "." + last_names[i].lower() + "@example.com"
-            tmp_pass = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(10))
+            tmp_email = (
+                first_names[i].lower() + "." + last_names[i].lower() + "@example.com"
+            )
+            tmp_pass = "".join(
+                random.choice(string.ascii_lowercase + string.digits) for i in range(10)
+            )
             user_datastore.create_user(
                 first_name=first_names[i],
                 last_name=last_names[i],
                 email=tmp_email,
                 password=hash_password(tmp_pass),
-                roles=[user_role, ]
+                roles=[
+                    user_role,
+                ],
             )
         db.session.commit()
     return
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
     # Build a sample db on the fly, if one does not exist yet.
     app_dir = os.path.realpath(os.path.dirname(__file__))
-    database_path = os.path.join(app_dir, app.config['DATABASE_FILE'])
+    database_path = os.path.join(app_dir, app.config["DATABASE_FILE"])
     if not os.path.exists(database_path):
         with app.app_context():
             build_sample_db()
 
     # Start app
-    app.run(host="0.0.0.0",port=8080)
+    app.run(host="0.0.0.0", port=8081)
